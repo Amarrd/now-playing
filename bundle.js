3e764beb18be5497b3ea25163fe444dfecf00710
Amar Rana-Deshmukh <amar.rd.dev@gmail.com>
2023-03-18 23:28:54 +0000

    added hue shift

--- bundle.js
+++ bundle.js
@@ -26493,6 +26493,7 @@ const Microphone = require("./microphone");
 
 var options = {
     hue: 350,
+    hueShift:5,
     volume: 100,
     curve: 60,
     zoom: 7,
@@ -26502,6 +26503,136 @@ var options = {
     speed: 2,
     bassMode: false
 }
+var maxV = 0;
+
+class Effect {
+    constructor(canvas) {
+        this.width = canvas.width;
+        this.height = canvas.height;
+        this.particles = [];
+        this.numberOfParticles = 1000;
+        this.cellSize = 20;
+        this.rows;
+        this.cols;
+        this.flowField = [];
+        this.curve = 0.3;
+        this.zoom = 0.1
+        this.counter = 0;
+        this.updateEffect(true, 0);
+
+        window.addEventListener('resize', e => {
+            let newWidth = e.target.innerWidth;
+            let newHeight = e.target.innerHeight;
+            canvas.width = newWidth;
+            canvas.height = newHeight;
+            this.width = canvas.width;
+            this.height = canvas.height;
+            this.updateEffect();
+        })
+    }
+
+    updateEffect(createParticles, volume) {
+        this.rows = Math.floor(this.height / this.cellSize);
+        this.cols = Math.floor(this.width / this.cellSize);
+        this.flowField = [];
+        //console.log('x:%d, y:%d', options.xAdjustment, options.yAdjustment)
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.cols; x++) {
+                let adjustedZoom = options.zoom/100
+                let angle = (Math.cos((x + this.counter * -options.xAdjustment) * adjustedZoom)
+                    + Math.sin((y + this.counter * options.yAdjustment) * adjustedZoom)) * (volume * options.curve/100);
+                this.flowField.push(angle);
+            }
+        }
+        this.counter += options.scrollSpeed/10;
+        // console.log('scrollSpeed: %f, counter:%f', options.scrollSpeed, this.counter)
+
+        if (createParticles) {
+            this.particles = [];
+            for (let i = 0; i < this.numberOfParticles; i++) {
+                this.particles.push(new Particle(this));
+            }
+        }
+    }
+
+    render(context, volume) {
+        this.particles.forEach(particle => {
+            particle.draw(context);
+            particle.updateParticle(volume);
+        })
+    }
+}
+
+class Particle {
+    constructor(effect) {
+        this.effect = effect;
+        this.x = Math.floor(Math.random() * this.effect.width);
+        this.y = Math.floor(Math.random() * this.effect.height);
+        this.speedX;
+        this.speedY;
+        this.speedModifier = 2;
+        this.history = [{ x: this.x, y: this.y }];
+        this.maxLength = Math.floor(Math.random() * 70 + 50);
+        this.angle = 0;
+        this.timer = this.maxLength * 2;
+        this.hue = options.hue; 
+        this.colours = [`hsl( ${this.hue}, 100%, 30%)`, `hsl( ${this.hue},100%,40%)`, `hsl( ${this.hue},100%, 50%)`];
+        this.colour = this.colours[Math.floor(Math.random() * this.colours.length)]
+    }
+    draw(context) {
+        context.beginPath();
+        context.moveTo(this.history[0].x, this.history[0].y)
+        for (let i = 0; i < this.history.length; i++) {
+            context.lineTo(this.history[i].x, this.history[i].y);
+        }
+
+        context.strokeStyle = this.colour;
+        context.stroke();
+
+    }
+
+    updateParticle(volume) {
+        this.timer--;
+        if (this.timer >= 1) {
+            let x = Math.floor(this.x / this.effect.cellSize);
+            let y = Math.floor(this.y / this.effect.cellSize);
+            let index = y * this.effect.cols + x;
+            this.angle = this.effect.flowField[index];
+
+            this.speedX = Math.cos(this.angle);
+            this.speedY = Math.sin(this.angle);
+
+            let randomSpeed = Math.floor(Math.random() * options.speed + 1);
+            this.x += this.speedX * (volume * randomSpeed + 2)
+            this.y += -this.speedY * (volume * randomSpeed + 2)
+
+            this.history.push({ x: this.x, y: this.y });
+
+            if (this.history.length > this.maxLength) {
+                this.history.shift();
+            }
+
+        } else if (this.history.length > 1) {
+            this.history.shift();
+
+        } else {
+            this.reset(volume);
+        }
+
+    }
+
+    reset(volume) {
+        this.x = Math.floor(Math.random() * this.effect.width);
+        this.y = Math.floor(Math.random() * this.effect.height);
+        this.hue = volume * options.hueShift + options.hue
+        //console.log(`volume:${volume}, hue:${this.hue}`)
+        this.colours = [`hsl( ${this.hue}, 100%, 30%)`, `hsl( ${this.hue},100%,40%)`, `hsl( ${this.hue},100%, 50%)`];
+        this.colour = this.colours[Math.floor(Math.random() * this.colours.length)]
+        this.history = [{ x: this.x, y: this.y }];
+        this.timer = this.maxLength;
+    }
+
+}
 
 function main(audioPromise) {
     const canvas = document.getElementById('myCanvas');
@@ -26509,181 +26640,52 @@ function main(audioPromise) {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     ctx.lineWidth = 1;
-    setOptions()
-
-    class Particle {
-        constructor(effect) {
-            this.effect = effect;
-            this.x = Math.floor(Math.random() * this.effect.width);
-            this.y = Math.floor(Math.random() * this.effect.height);
-            this.speedX;
-            this.speedY;
-            this.speedModifier = 2;
-            this.history = [{ x: this.x, y: this.y }];
-            this.maxLength = Math.floor(Math.random() * 70 + 50);
-            this.angle = 0;
-            this.timer = this.maxLength * 2;
-            this.hue = options.hue; 
-            this.colours = [`hsl( ${this.hue}, 100%, 30%)`, `hsl( ${this.hue},100%,40%)`, `hsl( ${this.hue},100%, 50%)`];
-            this.colour = this.colours[Math.floor(Math.random() * this.colours.length)]
-        }
-        draw(context) {
-            context.beginPath();
-            context.moveTo(this.history[0].x, this.history[0].y)
-            for (let i = 0; i < this.history.length; i++) {
-                context.lineTo(this.history[i].x, this.history[i].y);
-            }
-
-            context.strokeStyle = this.colour;
-            context.stroke();
-
-        }
-
-        updateParticle(volume) {
-            this.timer--;
-            if (this.timer >= 1) {
-                let x = Math.floor(this.x / this.effect.cellSize);
-                let y = Math.floor(this.y / this.effect.cellSize);
-                let index = y * this.effect.cols + x;
-                this.angle = this.effect.flowField[index];
-
-                this.speedX = Math.cos(this.angle);
-                this.speedY = Math.sin(this.angle);
-
-                let randomSpeed = Math.floor(Math.random() * options.speed + 1);
-                this.x += this.speedX * (volume * randomSpeed + 0.5)
-                this.y += this.speedY * (volume * randomSpeed + 0.5)
-
-                this.history.push({ x: this.x, y: this.y });
-
-                if (this.history.length > this.maxLength) {
-                    this.history.shift();
-                }
-
-            } else if (this.history.length > 1) {
-                this.history.shift();
-
-            } else {
-                this.reset(volume);
-            }
-
-        }
-
-        reset(volume) {
-            this.x = Math.floor(Math.random() * this.effect.width);
-            this.y = Math.floor(Math.random() * this.effect.height);
-            this.hue = volume * 5 + options.hue
-            //console.log(`volume:${volume}, hue:${this.hue}`)
-            this.colours = [`hsl( ${this.hue}, 100%, 30%)`, `hsl( ${this.hue},100%,40%)`, `hsl( ${this.hue},100%, 50%)`];
-            this.colour = this.colours[Math.floor(Math.random() * this.colours.length)]
-            this.history = [{ x: this.x, y: this.y }];
-            this.timer = this.maxLength;
-        }
-
-    }
-
-    class Effect {
-        constructor(canvas) {
-            this.width = canvas.width;
-            this.height = canvas.height;
-            this.particles = [];
-            this.numberOfParticles = 2000;
-            this.cellSize = 20;
-            this.rows;
-            this.cols;
-            this.flowField = [];
-            this.curve = 0.3;
-            this.zoom = 0.1
-            this.counter = 0;
-            this.updateEffect(true, 0);
-
-            window.addEventListener('resize', e => {
-                let newWidth = e.target.innerWidth;
-                let newHeight = e.target.innerHeight;
-                canvas.width = newWidth;
-                canvas.height = newHeight;
-                this.width = canvas.width;
-                this.height = canvas.height;
-                this.updateEffect();
-            })
-        }
-
-        updateEffect(createParticles, volume) {
-            this.rows = Math.floor(this.height / this.cellSize);
-            this.cols = Math.floor(this.width / this.cellSize);
-            this.flowField = [];
-            //console.log('x:%d, y:%d', options.xAdjustment, options.yAdjustment)
-            for (let y = 0; y < this.rows; y++) {
-                for (let x = 0; x < this.cols; x++) {
-                    let adjustedZoom = options.zoom/100
-                    let angle = (Math.cos((x + this.counter * -options.xAdjustment) * adjustedZoom)
-                        + Math.sin((y + this.counter * options.yAdjustment) * adjustedZoom)) * (volume * options.curve/100);
-                    this.flowField.push(angle);
-                }
-            }
-            this.counter += options.scrollSpeed/10;
-            // console.log('scrollSpeed: %f, counter:%f', options.scrollSpeed, this.counter)
-
-            if (createParticles) {
-                for (let i = 0; i < this.numberOfParticles; i++) {
-                    this.particles.push(new Particle(this));
-                }
-            }
-        }
-
-        render(context, volume) {
-            this.particles.forEach(particle => {
-                particle.draw(context);
-                particle.updateParticle(volume);
-            })
-        }
-    }
+    const microphone = new Microphone.Microphone(audioPromise);
+    const effect = new Effect(canvas);
+    effect.render(ctx, 0)
+    setOptions() 
+    animate();
 
     function animate() {
         if (microphone.initialised) {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
-            let normVolume = getNormalisedVolume()
+            let normVolume = getNormalisedVolume(microphone)
             effect.updateEffect(false, normVolume)
             effect.render(ctx, normVolume);
         }
         requestAnimationFrame(animate);
     }
+}
 
-    function getNormalisedVolume() {
-        if (options.bassMode) {
-            let samples = microphone.getSamples()
-            let sum = 0;
-            for (let i = 0; i< samples.length/10; i++) {
-                sum += samples[i];
-            }
-            var volume = sum / (samples.length/10);
+function getNormalisedVolume(microphone) {
+    if (options.bassMode) {
+        let samples = microphone.getSamples()
+        let sum = 0;
+        for (let i = 0; i< samples.length/10; i++) {
+            sum += samples[i];
         }
-        else {
-            var volume  = microphone.getVolume();
-        }
-        let minV = 0;
-        if (maxV < volume) {
-            maxV = volume;
-        }
-        if (volume < maxV * 0.2) {
-            maxV = volume;
-        }
-        let adjVolume = Math.floor(volume * options.volume) / 10;
-        let adjMaxV= maxV * 1.2
-        let normVolume = (adjVolume - minV) / (adjMaxV - minV);
-      //  console.log('vol:%f, max:%f, adj:%f, adjMax: %f, norm:%f', volume, maxV, adjVolume, adjMaxV, normVolume);
-        return normVolume
+        var volume = sum / (samples.length/10);
     }
-
-    const microphone = new Microphone.Microphone(audioPromise);
-    const effect = new Effect(canvas);
-    effect.render(ctx, 0)
-    let maxV = 0;
-    animate();
+    else {
+        var volume  = microphone.getVolume();
+    }
+    if (maxV < volume) {
+        maxV = volume;
+    }
+    if (volume < maxV * 0.1) {
+        maxV = volume;
+    }
+    let adjVolume = Math.floor(volume * options.volume) / 10;
+    let adjMaxV= maxV * 1.2
+    let minV = 0;
+    let normVolume = (adjVolume - minV) / (adjMaxV - minV);
+  //  console.log('vol:%f, max:%f, adj:%f, adjMax: %f, norm:%f', volume, maxV, adjVolume, adjMaxV, normVolume);
+    return normVolume
 }
 
 function setOptions() {
     document.querySelector('#hue').setAttribute('value', options.hue);
+    document.querySelector('#hueShift').setAttribute('value', options.hueShift);
     document.querySelector('#volume').setAttribute('value', options.volume);
     document.querySelector('#curve').setAttribute('value', options.curve);
     document.querySelector('#zoom').setAttribute('value', options.zoom);
@@ -26694,6 +26696,10 @@ function setOptions() {
 
 function hueChange(hue) {
     options.hue = hue;
+}
+
+function hueShiftChange(hueShift) {
+    options.hueShift = hueShift;
 }
 
 function volumeChange(volume) {
@@ -26725,8 +26731,8 @@ function toggleBassMode(bassMode) {
     console.log('bassMode:%s', options.bassMode)
 }
 
-module.exports = { main, hueChange, volumeChange, curveChange, zoomChange, xAdjustmentChange, 
-    yAdjustmentChange, scrollSpeedChange, toggleBassMode }
+module.exports = { main, hueChange, hueShiftChange, volumeChange, curveChange, zoomChange,
+    xAdjustmentChange,  yAdjustmentChange, scrollSpeedChange, toggleBassMode }
 },{"./microphone":191}],191:[function(require,module,exports){
 class Microphone {
     constructor(audioPromise) {
@@ -26964,6 +26970,10 @@ function hueChange() {
 	flowVisualiser.hueChange(Number(document.querySelector('#hue').value));
 }
 
+function hueShiftChange() {
+	flowVisualiser.hueShiftChange(Number(document.querySelector('#hueShift').value));
+}
+
 function volumeChange() {
 	flowVisualiser.volumeChange(Number(document.querySelector('#volume').value));
 }
@@ -26992,7 +27002,7 @@ function toggleBassMode() {
 	flowVisualiser.toggleBassMode(document.querySelector('#bassMode').checked);
 }
 
-module.exports = { startVisualiser, updateSong, toggleAuto, hueChange, volumeChange, 
+module.exports = { startVisualiser, updateSong, toggleAuto, hueChange, hueShiftChange, volumeChange, 
 	curveChange, zoomChange, xAdjustmentChange, yAdjustmentChange, scrollSpeedChange, toggleBassMode }
 },{"./acrCloud":187,"./barVisualiser":189,"./flowVisualiser":190,"audio-encoder":195}],193:[function(require,module,exports){
 var lamejs = require('lamejs');
