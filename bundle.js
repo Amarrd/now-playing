d95744e6fc682ceff1d0703988fee55e3ca6638c
Amar Rana-Deshmukh <37462651+Amarrd@users.noreply.github.com>
2023-04-01 23:46:10 +0100

    Temp change for rotation

--- bundle.js
+++ bundle.js
@@ -26728,7 +26728,7 @@ class FlowEffect {
             for (let x = 0; x < this.cols; x++) {
                 let adjustedZoom = this.options.zoom / 100
                 let angle = (Math.cos((x + this.counter * -this.options.xAdjustment) * adjustedZoom)
-                    + Math.sin((y + this.counter * this.options.yAdjustment) * adjustedZoom)) * (volume * this.options.curve / 10;
+                    + Math.sin((y + this.counter * this.options.yAdjustment) * adjustedZoom)) * (volume * this.options.curve / 100);
                 this.flowField.push(angle);
             }
         }
