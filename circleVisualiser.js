5e8d06ac96268c5d041de2c0ac6fac1a51c78fb0
Amar Rana-Deshmukh <amar.rd.dev@gmail.com>
2023-04-17 22:50:19 +0100

    Basic visualisation functionality

--- circleVisualiser.js
+++ circleVisualiser.js
@@ -2,10 +2,8 @@ const Microphone = require("./microphone");
 const p5 = require('p5');
 
 class CircleVisualiser {
-
     constructor(audioPromise) {
         this.microphone = new Microphone.Microphone(audioPromise);
-        this.hue = 100;
         this.setupControls();
         this.animate();
     }
@@ -21,44 +19,117 @@ class CircleVisualiser {
 
     animate() {
         const sketch = (s) => {
-            let hue = 0;
-            let inp;
+            let hue = 250;
+            let ringCount = 6;
+            let directionModifier = 1;
+            let dotModifier = 10;
+            let ringDistance = 100
+            let inp = s.createInput(hue, 'number');
+            let symmetrical = false;
+
             s.setup = () => {
-                s.createCanvas(window.innerWidth, window.innerHeight);
+                let canvas = s.createCanvas(window.innerWidth, window.innerHeight);
+                canvas.elt.setAttribute('onclick', 'myBundle.canvasClicked()')
                 s.noFill();
-                s.stroke(hue);
-                s.strokeWeight(8);
+               // s.stroke(s.color(`hsl(${hue}, 100%, 0%)`));
+                s.strokeWeight(2);
 
                 let label = document.createElement('label');
-                label.id='testLabel'
-                label.htmlFor='inp';
-                label.innerHTML='test';
+                label.id = 'hueLabel'
+                label.innerHTML = 'hue';
                 document.querySelector('#controls').appendChild(label);
-                inp = s.createInput(hue, 'number');
-                inp.position(0, 0);
-                inp.size(100);
-                inp.id('inp')
-                inp.elt.name='inp';
-                inp.parent('testLabel');
+                inp.parent('hueLabel');
                 inp.input(changeColour);
+                s.background(0)
+            }
+
+            s.draw = () => {
+                if (!this.microphone.initialised) {
+                    return;
+                }
+                s.clear();
+                let samples = this.microphone.getSampleSubArrays(ringCount, dotModifier);
+                let volume = s.map(this.microphone.getVolume(), 0, 0.5, 0, 2) + 0.3;
+                s.translate(window.innerWidth / 2, window.innerHeight / 2)
+                let currDot = 0;
+                for (let ringNumber = 1; ringNumber <= ringCount; ringNumber++) {
+                    let dotsForRing = ringNumber * dotModifier;
+                    let ringRadius = ringNumber * ringDistance;
+                    let samplesForRing = samples[ringNumber - 1]
+                    if (symmetrical) {
+                    } else {
+                        for (let angleIncrement = 0; angleIncrement < dotsForRing; angleIncrement++) {
+                            let noiseForCircle = s.noise(currDot + s.frameCount / 100);
+                            let adjustedNoise = noiseForCircle * volume;
+                            //console.log(`volume:${volume}, noise: ${noiseForCircle}, adjustedNoise:${adjustedNoise}`);
+                            directionModifier = (-2*(ringNumber%2)+1) // for alternating ring directions
+                            let angleOne = (s.frameCount / (Math.max(angleIncrement, 0.001) * 1000)) * -directionModifier - s.TWO_PI / dotsForRing;
+                            let xOne = ringRadius * Math.sin(angleOne * Math.max(angleIncrement, 0.001));
+                            let yOne = ringRadius * Math.cos(angleOne * Math.max(angleIncrement, 0.001));
+
+                            let dotSize = s.map(samplesForRing[angleIncrement], 0, 1, 30, 80 * ringNumber,true) * s.map(adjustedNoise, 0, 1, 1, 5);
+
+                            //s.map(s.noise(currDot + s.frameCount / 100), 0, 1, 10, 15);
+                            s.circle(xOne, yOne, dotSize);
+                            let maxHue = Number(hue) + 130;
+                            // let colour = Math.round(s.map(s.noise(currDot + s.frameCount / 100), 0, 1, Number(hue), maxHue));
+                            let colour = Math.round(s.map(samplesForRing[angleIncrement], 0, 0.5, Number(hue), maxHue, true) * s.map(adjustedNoise,0,1,0.75,1.25));
+                            //console.log(`sample:${samplesForRing[angleIncrement]}, colour:${colour}`)
+                            if (colour > 360) {
+                                colour = colour - 360;
+                            }
+                            // let colour = angleIncrement * 10;
+
+                            s.fill(s.color(`hsl(${colour}, 100%, 50%)`))
+                            currDot++;
+                        }
+                    }
+                   // console.log('ring:' + ringNumber + ',dotCount:' + currDot);
+                }
+              //  console.log(currDot);
+            }
+
+             let decay = (angleIncrement, ringNumber, adjustedNose) => {
+
+             }
+
+            s.windowResized = () => {
+                s.resizeCanvas(window.innerWidth, window.innerHeight);
             }
 
             let changeColour = () => {
                 hue = inp.value();
-                console.log(hue)
-             }
-
-            s.draw = () => {
-                s.background(0);
-                s.circle(window.innerWidth / 2, window.innerHeight / 2, 100);
-                s.fill(s.color(`hsl(${hue}, 100%, 50%)`))
             }
 
 
         }
-
         new p5(sketch);
     }
 }
+
+
+// for (let angleIncrement = 0; angleIncrement <= dotsForRing/2; angleIncrement++) {
+//     directionModifier = (-2*(ringNumber%2)+1) // for alternating ring directions
+//     let angleOne = (s.frameCount / (Math.max(angleIncrement, 0.001) * 1000)) * -directionModifier - s.TWO_PI / dotsForRing;
+//     let xOne = ringRadius * Math.sin(angleOne * Math.max(angleIncrement, 0.001));
+//     let yOne = ringRadius * Math.cos(angleOne * Math.max(angleIncrement, 0.001));
+//     let angleTwo = (s.frameCount / (Math.max(angleIncrement, 0.001) * 1000)) * -directionModifier + s.TWO_PI / dotsForRing;
+//     let xTwo = ringRadius * Math.sin(angleTwo * Math.max(angleIncrement, 0.001));
+//     let yTwo = ringRadius * Math.cos(angleTwo * Math.max(angleIncrement, 0.001));
+//     let dotSize = s.map(s.noise(currDot + s.frameCount / 100), 0, 1, 0, 100);
+//     s.circle(xOne, yOne, dotSize);
+//     s.circle(xTwo, yTwo, dotSize);
+//     let maxHue = Number(hue) + 130;
+//     let colour = Math.round(s.map(s.noise(currDot + s.frameCount / 100), 0, 1, Number(hue), maxHue));
+//     if (colour > 360) {
+//         colour = colour - 360;
+//     }
+//     if (ringNumber === 1){
+//         s.fill(s.color(`hsl(${colour}, 100%, 100%)`))
+//     } else {
+//         s.fill(s.color(`hsl(${colour}, 100%, 50%)`))
+//     }
+//     currDot++;
+// } 
 
 module.exports = { CircleVisualiser: CircleVisualiser };
--- microphone.js
+++ microphone.js
@@ -5,11 +5,12 @@ class Microphone {
             this.audioContext = new AudioContext();
             this.microphone = this.audioContext.createMediaStreamSource(stream);
             this.analyser = this.audioContext.createAnalyser();
-            this.analyser.fftSize = 512;
+            this.analyser.fftSize = 2048;
             const bufferLength = this.analyser.frequencyBinCount;
             this.dataArray = new Uint8Array(bufferLength);
             this.microphone.connect(this.analyser);
             this.initialised = true;
+            this.lastVolume = 0;
         }.bind(this)).catch(error => {
             console.log(error);
             alert(error);
@@ -23,15 +24,62 @@ class Microphone {
         return normSamples;
     }
 
+    getSampleSubArrays(arrayCount, elementModifier) {
+        let samples = this.getSamples();
+       // console.log(samples);
+        let perArray = Math.floor(samples.length/arrayCount);
+        let sampleArrays = [];
+        for (let i = 0; i < arrayCount; i++) {
+            let splitMin = i * perArray;
+            let splitMax = splitMin + perArray;
+          //  console.log(`array:${i}, min:${splitMin}, max:${splitMax}`)
+            let subSamples = [];
+            for (let j = splitMin; j < splitMax; j++) {
+            //    console.log(`j:${j}, sample:${samples[j]}`)
+                subSamples.push(samples[j]);
+            }
+            let arrayNumber = i+1;
+            let elementsPerArray = arrayNumber * ((arrayNumber+1)/2) * elementModifier + arrayNumber;
+            //console.log(`samplesBeforeReduction:`)
+            //console.log(subSamples);
+            sampleArrays.push(this.getReducedSamples(subSamples, elementsPerArray));
+        }
+        return sampleArrays;
+    }
+
+    getReducedSamples(samples, numberToSplit) {
+        if (numberToSplit > samples.length) numberToSplit = samples.length;
+        let perSplit = Math.floor(samples.length/numberToSplit);
+        let reducedSamples = [];
+        for (let i = 0; i < numberToSplit; i++) {
+            let splitMin = i * perSplit;
+            let splitMax = splitMin + perSplit;
+            let sum = 0;
+            for (let j = splitMin; j < splitMax; j++) {
+            //    console.log(samples[j])
+                sum += samples[j] * samples[j];
+            }
+            let result = 1-Math.sqrt(sum/perSplit);
+          //  console.log(`splitMin:${splitMin}, splitMax:${splitMax}, sum:${sum}, perSplit:${perSplit}, result:${result}`)
+            reducedSamples.push(result);
+        }
+     //   console.log(reducedSamples);
+        return reducedSamples;
+    }
+
     getVolume() {
         this.analyser.getByteTimeDomainData(this.dataArray);
-        let conversion = this.analyser.frequencyBinCount/2;
-        let normSamples = [...this.dataArray].map(e => e/conversion - 1);
+        let conversion = this.analyser.frequencyBinCount/4;
+        let normSamples = [...this.dataArray].map(e => e/conversion - 0.5);
         let sum = 0;
         for (let i = 0; i< normSamples.length; i++){
             sum += normSamples[i] * normSamples[i]
         }
-        return Math.sqrt(sum / normSamples.length)
+        let volume = Math.sqrt(sum / normSamples.length);
+        let volumeDiff = volume - this.lastVolume;
+        let smoothVolume = this.lastVolume + volumeDiff/2;
+        this.lastVolume = smoothVolume;
+        return smoothVolume;
     }
 }
 
