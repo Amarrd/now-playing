db43e4d09589d33b416d581b2f58d0e5eca7cb83
Amar Rana-Deshmukh <amar.rd.dev@gmail.com>
2023-04-21 01:24:21 +0100

    Tidied up code

--- circleVisualiser.js
+++ circleVisualiser.js
@@ -11,30 +11,34 @@ class CircleVisualiser {
     setupControls() {
         let container = document.querySelector('#controls-container');
         let profileSwitch = document.querySelector('#profileTransitionLabel');
-        profileSwitch.style.display = 'none';
-        container.style.opacity = 1;
         let height = (window.innerHeight - container.offsetHeight) / 2;
+
+        container.style.opacity = 1;
         container.style.top = height + 'px';
+        profileSwitch.style.display = 'none';
     }
 
     animate() {
         const sketch = (s) => {
-            let hue = 100;
-            let dotModifier = 2;
-            let ringCount = 8;
+            let baseHue = 10;
+            let hueShift = 30;
+            let dotModifier = 3;
+            let ringCount = 12;
             let directionModifier = 1;
             let ringDistance = 50
             let totalDots = 0;
-            let hueControl = s.createInput(hue, 'number');
+            let baseDotSize = 5;
+            let maxDotSize = 30;
+            let hueControl = s.createInput(baseHue, 'number');
             let dotCountControl = s.createInput(dotModifier, 'number');
             let ringCountControl = s.createInput(ringCount, 'number');
+            let dotSizes = [];
 
             s.setup = () => {
                 let canvas = s.createCanvas(window.innerWidth, window.innerHeight);
                 canvas.elt.setAttribute('onclick', 'myBundle.canvasClicked()')
                 s.noFill();
-                // s.stroke(s.color(`hsl(${hue}, 100%, 0%)`));
-                s.strokeWeight(2);
+                s.strokeWeight(1);
 
                 let hueLabel = document.createElement('label');
                 hueLabel.id = 'hueLabel'
@@ -64,41 +68,47 @@ class CircleVisualiser {
                 if (!this.microphone.initialised) {
                     return;
                 }
-                s.clear();
-                console.log(`totalDots:${totalDots}`)
+
                 let samples = this.microphone.getSamplesFor(totalDots);
                 let volume = s.map(this.microphone.getVolume(), 0, 0.5, 0, 2) + 0.3;
-                s.translate(window.innerWidth / 2, window.innerHeight / 2)
                 let currDot = 0;
+                s.clear();
+                s.translate(window.innerWidth / 2, window.innerHeight / 2)
+
                 for (let ringNumber = 1; ringNumber <= ringCount; ringNumber++) {
                     let dotsForRing = ringNumber * dotModifier;
                     let ringRadius = ringNumber * ringDistance;
-                    for (let angleIncrement = 0; angleIncrement < dotsForRing; angleIncrement++) {
-                        let noiseForCircle = s.noise(currDot + s.frameCount / 100);
-                        let adjustedNoise = noiseForCircle * volume;
-                        //console.log(`volume:${volume}, noise: ${noiseForCircle}, adjustedNoise:${adjustedNoise}`);
-                        directionModifier = (-2 * (ringNumber % 2) + 1) // for alternating ring directions
-                        let angle = (s.frameCount / (Math.max(angleIncrement, 0.001) * 1000)) * -directionModifier - s.TWO_PI / dotsForRing;
-                        let xOne = ringRadius * Math.sin(angle * Math.max(angleIncrement, 0.001));
-                        let yOne = ringRadius * Math.cos(angle * Math.max(angleIncrement, 0.001));
-                        let dotSize = s.map(samples[currDot], 0, 1, 10, 30 * ringNumber, true) * s.map(adjustedNoise, 0, 1, 1, 2, true);
-                        //s.map(s.noise(currDot + s.frameCount / 100), 0, 1, 10, 15);
-                        s.circle(xOne, yOne, dotSize);
-                        let maxHue = Number(hue) + 150;
-                        // let colour = Math.round(s.map(s.noise(currDot + s.frameCount / 100), 0, 1, Number(hue), maxHue));
-                        let colour = Math.round(s.map(samples[currDot], 0, 0.4, Number(hue), maxHue, true)); //* s.map(adjustedNoise, 0, 1, 0.75, 1.25));
-                        //console.log(`sample:${samplesForRing[angleIncrement]}, colour:${colour}`)
-                        if (colour > 360) {
-                            colour = colour - 360;
-                        }
-                        // let colour = angleIncrement * 10;
 
-                        s.fill(s.color(`hsl(${colour}, 100%, 50%)`))
+                    for (let angleIncrement = 0; angleIncrement < dotsForRing; angleIncrement++) {
+                        //directionModifier = (-2 * (ringNumber % 2) + 1) // for alternating ring directions
+                        let angle = (s.frameCount / (Math.max(angleIncrement, 0.001) * 1000)) * -directionModifier - s.TWO_PI / dotsForRing;
+                        let x = ringRadius * Math.sin(angle * Math.max(angleIncrement, 0.001));
+                        let y = ringRadius * Math.cos(angle * Math.max(angleIncrement, 0.001));
+                        let adjustedNoise = s.noise(currDot + s.frameCount / 100) * volume;
+                        let currentDotSize = dotSizes[currDot] || 0;
+                        let dotSize = s.map(samples[currDot], 0, 1, baseDotSize, maxDotSize * ringNumber, true) * s.map(adjustedNoise, 0, 1, 1, 2, true);
+                        let maxHue = Number(baseHue) + Number(hueShift);
+                        let hue = Math.round(s.map(samples[currDot], 0, 0.3, Number(baseHue), maxHue, true)); //* s.map(adjustedNoise, 0, 1, 0.75, 1.25));
+
+                        if (dotSize < currentDotSize) {
+                            dotSize = Math.max(currentDotSize * 0.98, baseDotSize);
+                        }
+                        
+                        if (hue > 360) {
+                            hue = hue - 360;
+                        }
+
+                        if (hue < 0) {
+                            hue = hue + 360;
+                        }
+                        
+                        s.circle(x, y, dotSize);
+                        s.fill(s.color(`hsl(${hue}, 100%, 50%)`))
+
+                        dotSizes[currDot] = dotSize;
                         currDot++;
                     }
-             //        console.log('ring:' + ringNumber + ',dotCount:' + currDot);
                 }
-                //   console.log(`loopCount:${currDot}`);
             }
 
             s.windowResized = () => {
@@ -106,7 +116,7 @@ class CircleVisualiser {
             }
 
             let changeColour = () => {
-                hue = hueControl.value();
+                baseHue = hueControl.value();
             }
 
             let changeDotCount = () => {
